6c6711ec707e7dae896bfe2c87ef817b4c80d6ef
RL4000 <lelong20004@gmail.com>
2022-10-23 23:34:35 +0700

    Init receipt
    add migration receipts and userDepts
    add model and controller and repo of receipt and userdepts
    add api get receipt by ID

---
+++ Capstone_API/Capstone_API/Controllers/UserDeptController.cs
@@ -0,0 +1,61 @@
+﻿using Capstone_API.Models;
+using Capstone_API.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Capstone_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDeptController : ControllerBase
+    {
+        private readonly IEventRepository repo;
+
+        public UserDeptController(IEventRepository eventRepository)
+        {
+            repo = eventRepository;
+        }
+        // GET: api/<UserDeptController>
+        [HttpGet]
+        public async Task<Respond<IEnumerable<Receipt>>> GetAllUserDepts([FromBody] User user)//NOT DONE
+        {
+            var events = repo.GetAllEventsAsync(user);
+            return new Respond<IEnumerable<Receipt>>()
+            {
+                StatusCode = HttpStatusCode.Accepted,
+                Error = "",
+                Message = "Get event success",
+                Data = null
+            };
+        }
+
+        // GET api/<UserDeptController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserDeptController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserDeptController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserDeptController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Capstone_API/Capstone_API/Repository/Interface/IReceiptRepository.cs
@@ -0,0 +1,15 @@
+﻿using Capstone_API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Capstone_API.Repository.Interface
+{
+    public interface IReceiptRepository
+    {
+        Task<List<Receipt>> GetReceiptByIDAsync(int ReceiptID);
+
+        Receipt GetReceiptByIDAsync2(int ReceiptID);
+
+
+    }
+}
