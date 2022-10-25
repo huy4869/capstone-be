e1052fb9e36e572e94f26a872127cea40e11167a
RL4000 <lelong20004@gmail.com>
2022-10-25 23:26:15 +0700

    Init get receipt

--- G24_BWallet_Backend/G24_BWallet_Backend/Migrations/20221022075450_UpdateEventUser.Designer.cs
+++ G24_BWallet_Backend/G24_BWallet_Backend/Migrations/20221022075450_UpdateEventUser.Designer.cs
@@ -19,7 +19,7 @@ namespace G24_BWallet_Backend.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 64)
                 .HasAnnotation("ProductVersion", "5.0.10");
 
-            modelBuilder.Entity("Capstone_API.Models.Account", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.Account", b =>
                 {
                     b.Property<int>("ID")
                         .ValueGeneratedOnAdd()
@@ -47,7 +47,7 @@ namespace G24_BWallet_Backend.Migrations
                     b.ToTable("Account");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.Event", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.Event", b =>
                 {
                     b.Property<int>("ID")
                         .ValueGeneratedOnAdd()
@@ -83,7 +83,7 @@ namespace G24_BWallet_Backend.Migrations
                     b.ToTable("Event");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.EventUser", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.EventUser", b =>
                 {
                     b.Property<int>("EventID")
                         .HasColumnType("int");
@@ -99,7 +99,7 @@ namespace G24_BWallet_Backend.Migrations
                     b.ToTable("EventUser");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.Friend", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.Friend", b =>
                 {
                     b.Property<int>("UserID")
                         .HasColumnType("int");
@@ -115,7 +115,7 @@ namespace G24_BWallet_Backend.Migrations
                     b.ToTable("Friend");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.Otp", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.Otp", b =>
                 {
                     b.Property<int>("OtpID")
                         .ValueGeneratedOnAdd()
@@ -141,7 +141,7 @@ namespace G24_BWallet_Backend.Migrations
                     b.ToTable("OtpCode");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.User", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.User", b =>
                 {
                     b.Property<int>("ID")
                         .ValueGeneratedOnAdd()
@@ -173,9 +173,9 @@ namespace G24_BWallet_Backend.Migrations
                     b.ToTable("User");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.EventUser", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.EventUser", b =>
                 {
-                    b.HasOne("Capstone_API.Models.Event", "Events")
+                    b.HasOne("G24_BWallet_Backend.Models.Event", "Events")
                         .WithMany()
                         .HasForeignKey("EventID")
                         .OnDelete(DeleteBehavior.Cascade)
@@ -184,18 +184,18 @@ namespace G24_BWallet_Backend.Migrations
                     b.Navigation("Events");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.Friend", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.Friend", b =>
                 {
-                    b.HasOne("Capstone_API.Models.User", null)
+                    b.HasOne("G24_BWallet_Backend.Models.User", null)
                         .WithMany("Friends")
                         .HasForeignKey("UserID")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.User", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.User", b =>
                 {
-                    b.HasOne("Capstone_API.Models.Account", "Account")
+                    b.HasOne("G24_BWallet_Backend.Models.Account", "Account")
                         .WithMany()
                         .HasForeignKey("AccountID")
                         .OnDelete(DeleteBehavior.Cascade)
@@ -204,7 +204,7 @@ namespace G24_BWallet_Backend.Migrations
                     b.Navigation("Account");
                 });
 
-            modelBuilder.Entity("Capstone_API.Models.User", b =>
+            modelBuilder.Entity("G24_BWallet_Backend.Models.User", b =>
                 {
                     b.Navigation("Friends");
                 });
---
+++ G24_BWallet_Backend/G24_BWallet_Backend/Models/Receipt.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace G24_BWallet_Backend.Models
+{
+    [Table("receipt")]
+    public class Receipt
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReceiptID { get; set; }
+
+        [ForeignKey("UserID")]
+        public User User { get; set; }
+        [ForeignKey("EventID")]
+        public Event Event { get; set; }
+
+
+        public string ReceiptName { get; set; }
+        public string ReceiptPicture { get; set; }
+        public int DivideType { get; set; }
+        public int ReceiptStatus { get; set; }
+        public double ReceiptAmount { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
