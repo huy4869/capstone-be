70c8b332687cd9c8b75b20346d28a7c79f84c0cc
RL4000 <lelong20004@gmail.com>
2022-10-27 23:47:21 +0700

    commit remove ignore

--- G24_BWallet_Backend/G24_BWallet_Backend/Controllers/ReceiptController.cs
+++ G24_BWallet_Backend/G24_BWallet_Backend/Controllers/ReceiptController.cs
@@ -1,7 +1,12 @@
-﻿using G24_BWallet_Backend.Models;
+﻿using G24_BWallet_Backend.DBContexts;
+using G24_BWallet_Backend.Models;
 using G24_BWallet_Backend.Models.ObjectType;
+using G24_BWallet_Backend.Repository;
 using G24_BWallet_Backend.Repository.Interface;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -18,65 +23,120 @@ namespace G24_BWallet_Backend.Controllers
     {
         private readonly IReceiptRepository receiptRepo;
         private readonly IUserDeptRepository deptRepo;
+        private readonly IEventUserRepository eventUserRepo;
 
-        public ReceiptController(IReceiptRepository InitReceiptRepo, IUserDeptRepository InitUserDeptRepo)
+        public ReceiptController(IReceiptRepository InitReceiptRepo, IUserDeptRepository InitUserDeptRepo, IEventUserRepository InitEventUserRepo)
         {
             receiptRepo = InitReceiptRepo;
             deptRepo = InitUserDeptRepo;
+            eventUserRepo = InitEventUserRepo;
         }
 
-        [HttpGet()]
-        public async Task<Respond<IEnumerable<Receipt>>> GetReceiptByEventIDUserID([FromQuery(Name = "eventid")] int eventid, [FromQuery(Name = "userid")] int userid)
+        [HttpGet]
+        public async Task<Respond<IEnumerable<Receipt>>> GetReceiptByEventID([FromQuery] int eventid)
         {
+            //nếu nhập link "api/receipt" báo thiếu trường tìm kiếm
+            if (String.IsNullOrEmpty(eventid.ToString()))
+                return new Respond<IEnumerable<Receipt>>()
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Error = "thiếu trường tìm kiếm",
+                    Message = "",
+                    Data = null
+                };
+
+            //thực hiện tìm kiếm theo event id
             var receipts = receiptRepo.GetReceiptByEventIDAsync(eventid) ;
-            
-            return new Respond<IEnumerable<Receipt>>()
+            if (receipts.Result.IsNullOrEmpty()) {
+                return new Respond<IEnumerable<Receipt>>()
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Error = "không tìm thấy các hóa đơn sự kiện",
+                    Message = "",
+                    Data = await receipts
+                };
+            }
+            else
             {
-                StatusCode = HttpStatusCode.Accepted,
-                Error = "",
-                Message = "lấy danh sách thành công",
-                Data = await receipts
-            };
+                return new Respond<IEnumerable<Receipt>>()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Error = "",
+                    Message = "lấy danh sách hóa đơn thành công",
+                    Data = await receipts
+                };
+            }
         }
 
 
         [HttpGet("{eventId}")]
-        public async Task<Respond<List<Receipt>>> GetReceipt(int eventId)//NOT DONE
+        public async Task<Respond<List<Receipt>>> GetReceipt(int eventId)
         {
-            var r = receiptRepo.GetReceiptByEventIDAsync(eventId);
 
-            return new Respond<List<Receipt>>()
-            {
-                StatusCode = HttpStatusCode.Accepted,
-                Error = "",
-                Message = "Get receipt success",
-                Data = await r
-            };
+            var r = receiptRepo.GetReceiptByEventIDAsync(1);
+
+            if(r == null){
+                return new Respond<List<Receipt>>()
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Error = "không tìm thấy hóa đơn",
+                    Message = "",
+                    Data = await r
+                };
+            } else {
+                return new Respond<List<Receipt>>()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Error = "",
+                    Message = "tìm thấy hóa đơn",
+                    Data = await r
+                };
+            }
         }
-        /*
-        // GET api/<ReceiptController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)//NOT DONE
-        {
-            return "value";
-        }*/
+
 
         //create receipt
-        // POST api/<ReceiptController>
+        [HttpGet("create")]
+        public async Task<Respond<List<Member>>> PrepareCreateReceipt([FromQuery] string eventid)
+        {
+            int CheckEventID;
+            bool isNumeric = int.TryParse(eventid, out CheckEventID);
+            if (!isNumeric)
+            {
+                return new Respond<List<Member>>()
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Error = "sai event id",
+                    Message = "",
+                    Data = null
+                };
+            }
+
+            var eventUsers = eventUserRepo.GetAllEventUsersAsync(CheckEventID);
+
+            return new Respond<List<Member>>()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Error = "",
+                Message = "lấy danh sách thành viên trong event thành công",
+                Data = await eventUsers
+            };
+        }
+
         [HttpPost("create")]
-        public async Task<Respond<ArrayList>> CreateReceipt([FromForm] Receipt receipt /*List<User> involve_members = null, List<UserDept> userDepts = null*/)
+        public async Task<Respond<ArrayList>> PostCreateReceipt([FromBody] Receipt receipt, List<UserDept> involve_members = null)
         {//note all create will have status not approve
 
             //repo.createReceipt
-            //switch(receipt)
-            //foreach(userDepts)
+            //
+            //foreach(users)
             //deptRepo.createDept
             //end for
             return new Respond<ArrayList>()
             {
                 StatusCode = HttpStatusCode.Created,
                 Error = "",
-                Message = "Testing",
+                Message = "create receipt not done",
                 Data = new ArrayList { "name: " + receipt.ReceiptName + ",receiptUserID: " + receipt.UserID}
             };
         }
--- G24_BWallet_Backend/G24_BWallet_Backend/Models/Receipt.cs
+++ G24_BWallet_Backend/G24_BWallet_Backend/Models/Receipt.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace G24_BWallet_Backend.Models
 {
@@ -17,12 +18,14 @@ namespace G24_BWallet_Backend.Models
         [ForeignKey("Event")]
         public int EventID { get; set; }
 
-
         public string ReceiptName { get; set; }
         public string ReceiptPicture { get; set; }
-        public int DivideType { get; set; }
         public int ReceiptStatus { get; set; }
         public double ReceiptAmount { get; set; }
+
+        public ICollection<UserDept> listUserDept { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
