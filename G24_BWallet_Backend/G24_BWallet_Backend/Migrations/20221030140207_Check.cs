fd9fd9c573110a14bfd9caa0d976068305b76c75
duybeoo <nqd3092@gmail.com>
2022-10-30 21:04:11 +0700

    Code moi nhat

---
+++ G24_BWallet_Backend/G24_BWallet_Backend/Migrations/20221030140207_Check.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace G24_BWallet_Backend.Migrations
+{
+    public partial class Check : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
