7ad51b95c2b8d0f55f2b18021fd6f65dbacf6312
duybeoo <nqd3092@gmail.com>
2022-11-02 21:54:17 +0700

    Delete all file(wrong)

--- G24_BWallet_Backend/G24_BWallet_Backend/Models/ObjectType/UserPhone.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace G24_BWallet_Backend.Models.ObjectType
-{
-    public class UserPhone
-    {
-        public User User { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
