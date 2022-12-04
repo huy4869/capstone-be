92395ee9118f33ee7d93eabe4692e179f2e428f9
duybeoo <nqd3092@gmail.com>
2022-12-04 14:49:20 +0700

    Remind debt by SMS

--- G24_BWallet_Backend/G24_BWallet_Backend/Repository/Interface/IDebtReceiveDetailRepo.cs
+++ G24_BWallet_Backend/G24_BWallet_Backend/Repository/Interface/IDebtReceiveDetailRepo.cs
@@ -8,5 +8,6 @@ namespace G24_BWallet_Backend.Repository.Interface
         Task<ReceiptUserDeptName> ClickIButton(int receiptId, int v);
         Task<TotalMoneyUser> GetAllDebtInEvent(int userId, int eventId);
         Task<TotalMoneyUser> GetAllReceiveInEvent(int v, int eventId);
+        Task<string> SendRemind(IdAvatarNamePhoneMoney i);
     }
 }
