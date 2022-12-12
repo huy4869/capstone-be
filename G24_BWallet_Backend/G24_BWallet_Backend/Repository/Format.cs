898e1e024f056a729180d2a636c41d58b6544064
RL4000 <lelong20004@gmail.com>
2022-12-12 22:51:14 +0700

    update format

--- G24_BWallet_Backend/G24_BWallet_Backend/Repository/Format.cs
+++ G24_BWallet_Backend/G24_BWallet_Backend/Repository/Format.cs
@@ -38,10 +38,11 @@ namespace G24_BWallet_Backend.Repository
 
         public string MoneyFormat(double money)
         {
-
-            // return money.ToString("C0");
-            var info = System.Globalization.CultureInfo.GetCultureInfo("vi-VN");
-            return String.Format(info, "{0:c}", money);
+            //return money.ToString("{0:c}") + " ₫";
+            //var info = System.Globalization.CultureInfo.GetCultureInfo("vi-VN");
+            //return String.Format(info, "{0:c}", money);
+            NumberFormatInfo nfi = new CultureInfo("vi-VN", false).NumberFormat;
+            return money.ToString("C0",nfi);
         }
         public string Reverse(string s)
         {
